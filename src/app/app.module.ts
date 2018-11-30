82713ba5b37f8b296738c7050e19c5057921feb2
Vocalnight <vocalebooks@gmail.com>
2018-11-30 17:34:05 -0200

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ChooserComponent } from './chooser/chooser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ChooserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
